5f92d7ce394598a2f27728943206f643c383fe73
Kaique Miceli <163534270+kaiquemicelidev@users.noreply.github.com>
2024-03-16 12:43:59 -0300

    Novo update para escrita da lista de cupons

--- Components/script/api.js
+++ Components/script/api.js
@@ -1,4 +1,6 @@
 $("#button_go").on('click', function(){
+
+    $(".cpnList").html("");
     
     $.ajax({
         url: 'http://localhost/Cupom-na-Mao/API/index.php?type=search&date=' + 
@@ -7,7 +9,38 @@ $("#button_go").on('click', function(){
         type: 'GET',
         dataType: 'json',
         success: function(Result) {
-            
+            if(Result.FileExists){ //O arquivo existe
+                console.log('Existe');
+                let ListCount = Result.ListCount;
+                $(".cpnList").append(
+                    "<div class='ListCount'>Número de Resultados: " + Result.ListCount + "</div>"
+                );
+                if(Result.ListCount > 0){
+                    let CupomList = Result.CupomList;
+                    CupomList.forEach(Cupom => {
+                        let CupomKey = Cupom.CupomKey;
+                        let CupomIn = Cupom.Prop.CupomIn;
+                        let CupomOut = Cupom.Prop.CuponOut;
+                        let CupomPay = Cupom.Prop.CupomPay;
+                        let CupomValue = Cupom.Prop.CupomValue;
+                        let CupomDate = Cupom.Prop.CupomDate;
+
+                        $(".cpnList").append(
+                            "<div class='CupomItem'>" +
+                            "<h3>" + CupomKey + "</h3>" +
+                            "<h4>" + CupomValue + "</h4>" +
+                            "<h4>" + CupomPay + "</h4>" +
+                            "<h4>" + CupomDate + "</h4>" 
+                            );
+                        
+                    });
+                }
+
+            }else{ // O arquivo não existe
+                $(".cpnList").append(
+                    "<div class='ListCount'>ArqEspelho inexistente</div>"
+                );
+            }
             
         }
     })
